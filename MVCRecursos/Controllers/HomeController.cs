a8b6c1318e6d5df84d36bbf3f830b11f262ec1aa
jgiganto <jgiganto1@hotmail.com>
2018-03-01 13:18:16 +0100

    s

--- MVCRecursos/Controllers/HomeController.cs
+++ MVCRecursos/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+ 
 
 namespace MVCRecursos.Controllers
 {
@@ -10,7 +11,9 @@ namespace MVCRecursos.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Saludo = Resources.Saludos.Titulo;
             return View();
+           
         }
 
         public ActionResult About()
